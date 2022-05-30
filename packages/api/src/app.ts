0a1c4adb7119c505c29da2a0ce0c3237fc0a78ef
Gabriel Valeriano <53843213+Lummiell@users.noreply.github.com>
2022-05-30 19:26:24 -0300

    Remove route logging function

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,10 +1,6 @@
 import express from 'express';
 import { routes } from './Routes';
 const app = express();
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
 app.use(express.json());
 app.use(routes);
 
