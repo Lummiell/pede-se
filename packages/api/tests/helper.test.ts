b17714c78c0dae42c9d46a831ecaad356b89d821
Gabriel Valeriano <53843213+Lummiell@users.noreply.github.com>
2022-06-27 00:37:08 -0300

    Add timeout and maximum connection attempts to before() hook on API tests

--- packages/api/tests/helper.test.ts
+++ packages/api/tests/helper.test.ts
@@ -2,16 +2,24 @@ import 'reflect-metadata';
 import { setupConnection, dropConnection } from './Integration/util';
 import { wait } from '../src/Util/wait';
 
-before(() => {
+const waitMs = 2000;
+const maxAttempts = 10;
+before(function () {
+  this.timeout(2000 + waitMs * maxAttempts);
   const hook = async () => {
     let connected = false;
+    let attempts = 0;
     while (!connected) {
       try {
         await setupConnection();
         connected = true;
-      } catch (e) {
-        const waitMs = 5000;
+      } catch (error) {
+        console.error(error);
         console.log(`Retrying to connect in ${waitMs / 1000}s`);
+        attempts++;
+        if (attempts >= maxAttempts) {
+          throw error;
+        }
         await wait(waitMs);
       }
     }
