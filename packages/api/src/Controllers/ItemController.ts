6c28b5e402503d76815dbe805e23d593f30b7ef2
Gabriel Valeriano <53843213+Lummiell@users.noreply.github.com>
2022-07-03 23:14:10 -0300

    Fix Item POST method with wrong property destructuring name; Fix wrong status code in the same method

--- packages/api/src/Controllers/ItemController.ts
+++ packages/api/src/Controllers/ItemController.ts
@@ -16,16 +16,16 @@ const ItemController = {
     }
   },
   post: async (req: Request, res: Response) => {
-    const { name, price, group } = req.body;
+    const { name, price, itemGroupID } = req.body;
     const item = new Item();
     item.name = name;
     item.price = price;
-    if (group) {
-      const groupObj = await ItemGroupService.findOne(group);
+    if (itemGroupID) {
+      const groupObj = await ItemGroupService.findOne(itemGroupID);
       if (groupObj) {
         item.group = groupObj;
       } else {
-        return res.status(404).json({ error: 'ItemGroupNotFound' });
+        return res.status(409).json({ error: 'ItemGroupNotFound' });
       }
     }
     const itemInserted = await ItemService.create(item);
